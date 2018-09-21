d6aeb758c47be3c9401ee920e419a534d6062251
Nick Harris <nharris85@gmail.com>
2018-09-21 13:39:16 -0600

    gql resolver works with espn fetch

--- handlers/graphql.js
+++ handlers/graphql.js
@@ -1,6 +1,14 @@
-import { server } from "../lib/graphql";
+import { createServer } from "../lib/graphql";
+
+const server = createServer(
+  process.env.ESPN_S3_COOKIE,
+  process.env.SWID_COOKIE,
+  process.env.LEAGUE_ID,
+  process.env.SEASON
+);
 
 export const query = (event, context, callback) => {
+  console.log("LEAGUE_ID: ", process.env.LEAGUE_ID);
   const handler = server.createHandler({
     cors: {
       origin: "*",
